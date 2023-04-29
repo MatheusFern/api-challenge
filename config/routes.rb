9ca9e0817d608ef36683a878340be53acd8088ad
Leonardo Quevedo <leonardoq@trin.ca>
2023-04-29 11:23:48 -0300

    Login Endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth/v1/user'
+
+  namespace :admin do
+    namespace :v1 do
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
